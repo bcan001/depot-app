5f72994eca6ff1c8642314bea45ce0a71e008a22
Ben Caneba <bcaneba@gmail.com>
2015-06-04 16:59:20 -0500

    validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ActiveRecord::Base
+	# validate that these fields exist upon creation of a product:
+	validates(:title, :description, :image_url, presence: true)
+	# validate that price is a number upon product creation
+	validates(:price, numericality: {greater_than_or_equal_to: 0.01})
+	# validate that each product title is unique upon creation
+	validates(:title, uniqueness: true)
+	# validate that the URL added for the image does exist
+	validates(:image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG, or PNG image'
+	})
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,64 @@
 require 'test_helper'
 
+# VALIDATION TESTING (UNIT TESTING FOR MODELS)
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	# load our fixtures file (products.yml file)
+	fixtures :products
+	# test validation of unique product titles:
+	test "product is not valid without a unique title" do
+		product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
+		assert product.invalid?
+		assert_equal ["has already been taken"], product.errors[:title]
+	end
+
+
+  test "product attributes must not be empty" do
+  	product = Product.new
+  	assert product.invalid?
+  	assert product.errors[:title].any?
+  	assert product.errors[:description].any?
+  	assert product.errors[:price].any?
+  	assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+  	# product = Product.new(title: "My Book Title", description: "yyy", image_url: "zzz.jpg")
+  	product = products(:ruby)
+
+  	product.price = -1
+  	assert product.invalid?
+  	assert_equal(["must be greater than or equal to 0.01"], product.errors[:price])
+
+  	product.price = 1
+  	assert product.valid?
+  end
+
+
+
+  # test that the validating image URL ends with gif, png, or jpg
+  def new_product(image_url)
+  	Product.new(title: "My Book Title", description: "yyy", image_url: image_url)
+  end
+  test "image_url" do
+  	ok = %w{ fred.gif fred.jpg fred.png }
+  	bad = %w{ fred.doc fred.xyz fred.jjj }
+  	ok.each do |name|
+  		assert new_product(name).valid?, "#{name} should be valid"
+  	end
+  	bad.each do |name|
+  		assert new_product(name).invalid?, "#{name} should'nt be valid"
+  	end
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
