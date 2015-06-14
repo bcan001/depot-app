35c2ac579fb13a2d2d40540dcd9454c6c4abdf2a
Ben Caneba <bcaneba@gmail.com>
2015-06-14 16:17:04 -0700

    added cart quantity functionality:

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,15 @@
 class Cart < ActiveRecord::Base
 	# line_items contained in the cart will be destroyed if the Cart is destroyed
 	has_many :line_items, dependent: :destroy
+
+	# if there is already the same item in the cart, increment the quantity by 1
+	def add_product(product_id)
+		current_item = line_items.find_by(product_id: product_id)
+		if current_item
+			current_item.quantity += 1
+		else
+			current_item = line_items.build(product_id: product_id)
+		end
+		current_item
+	end
 end
